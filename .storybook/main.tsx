7add3f30101a22342e6d3749dddfe2a76d9e6235
Phil Lackey <plackey@costar.com>
2021-02-17 11:24:45 -0600

    fixed storybook build issues

--- .storybook/main.tsx
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: [
-    // Paths to the story files
-    '../pages/*.stories.mdx',
-    '../pages/*.stories.js',
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-};
